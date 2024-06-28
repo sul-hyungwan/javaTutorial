2d03048ce4edec8b23e403b9c82a1760974c717e
sul-hyungwan <tjfgusrhks94@skkukdp.re.kr>
2024-06-28 17:48:16 +0900

    chapter09
    수업자료

---
+++ chapter09_this/src/org/joonzis/ex/Ex01_Rect.java
@@ -0,0 +1,22 @@
+package org.joonzis.ex;
+
+public class Ex01_Rect {
+	int width, height;
+	
+	public Ex01_Rect() {}
+	public Ex01_Rect(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	void output() {
+		System.out.println("너비 : " + width);
+		System.out.println("높이 : " + height);
+	}
+	
+}
